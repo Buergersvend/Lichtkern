d451c3bf4cdae13cc81ed5be6ce93aa80aa6449b
Buergersvend <donath-sven@web.de>
2026-03-07 22:11:52 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../Lichtkern.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
