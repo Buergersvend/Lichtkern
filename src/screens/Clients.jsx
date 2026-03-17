7972954ef1696ce52fc740eca5bb00f6b3537f86
Buergersvend <donath-sven@web.de>
2026-03-17 18:36:59 +0100

    Update Clients.jsx

--- src/screens/Clients.jsx
+++ src/screens/Clients.jsx
@@ -3,7 +3,7 @@ import { T } from "../config/theme.js";
 import { Flower } from "../components/Decorations";
 import { Card, Btn, TI, Select, Pill, SL } from "../components/UI.jsx";
 
-import { BodygraphSVG, HDTab } from "../components/HumanDesign.jsx";
+import { BodygraphSVG, HDTab, HD_CHANNELS, HD_CENTER_CFG } from "../components/HumanDesign.jsx";
 
 function ClientDetailModal({client,sessions,onClose,onSave,onStart,onAnalyse}){
   const [tab,setTab]=useState('profil');
