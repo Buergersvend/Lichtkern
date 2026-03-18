709085131f55511e3330ee4c3c5c478d83168a35
Buergersvend <donath-sven@web.de>
2026-03-18 21:19:09 +0100

    Update Session.jsx

--- src/screens/Session.jsx
+++ src/screens/Session.jsx
@@ -4,7 +4,7 @@ import { Flower } from "../components/Decorations";
 import { T } from "../config/theme.js";
 import { APPT_TYPES, LEVELS, TECHNIQUES, KNOWLEDGE, DE_DAYS, DE_DAYS_F, DE_MONTHS, HOURS } from "../config/constants.js";
 import { lvl, top2, dynGrad, uid } from "../config/helpers";
-import { Card, Btn, TI, Select, LBar, Pill, SL } from "../components/UI.jsx";
+import { Card, Btn, Select, LBar, Pill, SL } from "../components/UI.jsx";
 import { PDFModal } from "./PDFModal.jsx";
 const STEPS = ["Klient","Ziel","Ebenen","Techniken","Abschluss"];
 
