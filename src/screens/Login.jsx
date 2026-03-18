a0c0ac5316085a5a9bc1889df4f0d2063fd32fd0
Buergersvend <donath-sven@web.de>
2026-03-18 21:47:15 +0100

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { T } from "../config/theme.js";
-import { Flower, TreeOfLife, TI, Btn } from "../components/UI.jsx";
+import { Flower, TI, Btn } from "../components/UI.jsx";
 import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "../config/firebase.js";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 const db = getFirestore();
