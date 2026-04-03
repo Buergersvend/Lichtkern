dbada83804bcdd282120db1dc15db4cc396e8434
Buergersvend <donath-sven@web.de>
2026-04-03 12:05:30 +0200

    Update History.jsx

--- src/screens/History.jsx
+++ src/screens/History.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Flower } from "../components/Decorations";
 import { T } from "../config/theme.js";
-import { Card, Btn } from "../components/UI.jsx";
+import { Card, Btn, SL, TI } from "../components/UI.jsx";
 import { top2, lvl, dynGrad } from "../config/helpers";
 function History({sessions, onDelete}){
   const [detail,setDetail]=useState(null);
