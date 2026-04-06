1e9552c14ac3848ef8dc08483adbd60034290804
Buergersvend <donath-sven@web.de>
2026-04-06 10:03:23 +0200

    Update History.jsx

--- src/screens/History.jsx
+++ src/screens/History.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Flower } from "../components/Decorations";
 import { T } from "../config/theme.js";
-import { Card, Btn, SL, TI } from "../components/UI.jsx";
+import { Card, Btn, SL, TI, LBar } from "../components/UI.jsx";
 import { top2, lvl, dynGrad } from "../config/helpers";
+import { LEVELS } from "../config/constants";
 function History({sessions, onDelete}){
   const [detail,setDetail]=useState(null);
   const [pdfSession,setPdfSession]=useState(null);
